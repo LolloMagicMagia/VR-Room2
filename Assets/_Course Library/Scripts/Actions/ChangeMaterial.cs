12d7daa72ce302c3607c33833d30f3e09ebdf494
lorenzo monti <95172711+LolloMagicMagia@users.noreply.github.com>
2024-07-09 10:34:11 +0200

    Initial commit

---
+++ Assets/_Course Library/Scripts/Actions/ChangeMaterial.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This script makes it easier to toggle between a new material, and the objects original material.
+/// </summary>
+public class ChangeMaterial : MonoBehaviour
+{
+    [Tooltip("The material that's switched to.")]
+    public Material otherMaterial = null;
+
+    private bool usingOther = false;
+    private MeshRenderer meshRenderer = null;
+    private Material originalMaterial = null;
+
+    private void Awake()
+    {
+        //prende riferimento al material e nel meshrender del telecomando
+        meshRenderer = GetComponent<MeshRenderer>();
+        //va a prendere il primo materiale
+        originalMaterial = meshRenderer.material;
+    }
+
+    public void SetOtherMaterial()
+    {
+        usingOther = true;
+        meshRenderer.material = otherMaterial;
+    }
+
+    public void SetOriginalMaterial()
+    {
+        usingOther = false;
+        meshRenderer.material = originalMaterial;
+    }
+
+    public void ToggleMaterial()
+    {
+        usingOther = !usingOther;
+
+        if(usingOther)
+        {
+            meshRenderer.material = otherMaterial;
+        }
+        else
+        {
+            meshRenderer.material = originalMaterial;
+        }
+    }
+}
